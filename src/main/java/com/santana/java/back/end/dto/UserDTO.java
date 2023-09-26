0374eff08ae0f98100fe43ac3ce396a431440224
Clinton <clinton.almeida10@gmail.com>
2023-09-26 08:46:15 -0300

    Primeiro commit

---
+++ src/main/java/com/santana/java/back/end/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package com.santana.java.back.end.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    
+    @NotBlank(message = "Nome é obrigatorio")
+    private String nome;
+    @NotBlank(message = "CPF é obrigatorio")
+    private String cpf;
+    private String endereco;
+    @NotBlank(message = "Email é obrigatorio")
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+}
